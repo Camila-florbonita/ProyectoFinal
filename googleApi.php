f294b8513491ea1911f31ef247ad7817f5a5c739
Camila-florbonita <fotosdecamilarock67@gmail.com>
2022-04-14 01:18:17 -0500

    Tercer commit

--- googleApi.php
+++ googleApi.php
@@ -1,12 +1,84 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="styleLogin.css">
-        <title>Iniciar sesion</title>
-    </head>
-    <body>
+<?php
 
-        <header class="header">
+//index.php
+
+//Include Configuration File
+include('loginGoogle.php');
+
+$login_button = '';
+
+
+if(isset($_GET["code"]))
+{
+
+ $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
+
+
+ if(!isset($token['error']))
+ {
+ 
+  $google_client->setAccessToken($token['access_token']);
+
+ 
+  $_SESSION['access_token'] = $token['access_token'];
+
+  $google_service = new Google_Service_Oauth2($google_client);
+
+ 
+  $data = $google_service->userinfo->get();
+
+ 
+  if(!empty($data['given_name']))
+  {
+   $_SESSION['user_first_name'] = $data['given_name'];
+  }
+
+  if(!empty($data['family_name']))
+  {
+   $_SESSION['user_last_name'] = $data['family_name'];
+  }
+
+  if(!empty($data['email']))
+  {
+   $_SESSION['user_email_address'] = $data['email'];
+  }
+
+  if(!empty($data['gender']))
+  {
+   $_SESSION['user_gender'] = $data['gender'];
+  }
+
+  if(!empty($data['picture']))
+  {
+   $_SESSION['user_image'] = $data['picture'];
+  }
+ }
+}
+
+
+if(!isset($_SESSION['access_token']))
+{
+
+ $login_button = '<a href="'.$google_client->createAuthUrl().'"><button type="button" style="background:#4285f4; margin-top: 15px; margin-bottom: 15px; color:white; border:none; width:300px; height:40px; border-radius:3%;"><img src="googleIcono.png" style="width:30px; background:white; border-radius:50%;" alt=""><b style="top: auto; left: 15px; position: relative">Inicia sesión con Google</b></button></a></a>';
+}
+
+?>
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <!-- <title>PHP Login using Google Account</title> -->
+  <title>Iniciar sesion</title>
+  <link rel="stylesheet" href="styleLogin.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+ 
+ </head>
+ <body>
+ <header class="header">
             <img class="logo" src="logo_provisional.jpg">
             <form class="buscar" action="#">
                 <input class="busqueda" type="text" name="busqueda" placeholder="Buscar...">
@@ -40,7 +112,7 @@
                     Iniciar sesión
                 </button>
 
-                <!-- <div class="panel panel-default">
+                <div class="panel panel-default">
                     <?php
                     if($login_button == '')
                     {
@@ -55,7 +127,7 @@
                      echo '<div align="center">'.$login_button . '</div>';
                     }
                     ?>
-                    </div> -->
+                    </div>
                     <!-- <button class="botonLoginGoogle">
                         Iniciar con Google
                     </button> -->
@@ -88,5 +160,5 @@
                 No se que mas vaya aqui :P
             </p>
         </footer>
-    </body>
+ </body>
 </html>
