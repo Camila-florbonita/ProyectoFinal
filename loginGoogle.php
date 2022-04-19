1abb0a83694526ce6c2761f6373f0add51417efc
Camila-florbonita <fotosdecamilarock67@gmail.com>
2022-04-19 01:01:08 -0500

    Cuarto commit

--- loginGoogle.php
+++ loginGoogle.php
@@ -18,7 +18,7 @@ $google_client->setClientId('614500226004-kj43csb97d21edr7ahb47f1pgg67dmp7.apps.
 $google_client->setClientSecret('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');
 
 //Set the OAuth 2.0 Redirect URI
-$google_client->setRedirectUri('http://localhost/ProyectoFinal/login.html');
+$google_client->setRedirectUri('http://localhost/ProyectoFinal/InicioConCuenta.html');
 
 // to get the email and profile 
 $google_client->addScope('email');
