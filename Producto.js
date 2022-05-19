2735c08bd91cd3e91710ea12ead3d99b00e784e5
Camila-florbonita <fotosdecamilarock67@gmail.com>
2022-05-19 00:02:02 -0500

    hosting

--- Producto.js
+++ Producto.js
@@ -44,3 +44,45 @@ req3.onload = function InfoProducto3()
 
 }, false);
 
+document.addEventListener('DOMContentLoaded', 
+function paypal()
+{
+    var paypal_button_container = document.getElementById("paypal-button-container");
+
+    paypal.Buttons({
+        style: {
+            color: 'blue',
+            shape: 'pill',
+            label: 'pay'
+        },
+        createOrder: function(data, actions)
+        {
+            return actions.order.create(
+                {
+                    purchase_units: [{
+                        amount: {
+                            value: productPrice
+                        }
+                    }]
+                });
+        },
+
+        onApprove: function(data, actions)
+        {
+            actions.order.capture().then(function(detalles)
+            {
+                console.log(detalles);
+                alert("Pago realizado");
+                //window.location.href="";
+            });
+        },
+
+        onCancel: function(data)
+        {
+            alert("Pago cancelado");
+            console.log(data);
+        }
+    }).render(paypal_button_container);
+
+}, false);
+
