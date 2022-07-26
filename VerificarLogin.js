a344df0599f3b8b9968e043da7f618b9f2f9a573
Camila-florbonita <fotosdecamilarock67@gmail.com>
2022-07-26 14:14:58 -0500

    Verificacion de inicio de sesion

--- VerificarLogin.js
+++ VerificarLogin.js
@@ -1,11 +1,46 @@
-fetch('./VerificarLogin.php')
-    .then(function (res) {
-        return res.text();
-    }).then(function (text) {
-        var datos = JSON.parse(text);
+// fetch('./VerificarLogin.php')
+//     .then(function (res) {
+//         return res.text();
+//     }).then(function (text) {
+//         var datos = JSON.parse(text);
         
-        if (datos.id_us == "0") {
+//         if (datos.id_us == "0") {
+//             window.location.replace("./login.html");
+//         }
+        
+//     });
+
+var VerifyLogin = new XMLHttpRequest();
+
+document.addEventListener('DOMContentLoaded', 
+function VerificarLogin()
+{
+    VerifyLogin.onload = function Verification()
+    {
+        var sesion;
+        sesion = VerifyLogin.responseText;
+        if (sesion == 0)
+        {
             window.location.replace("./login.html");
         }
-        
-    });
+    }
+    VerifyLogin.open("GET", "VerificarLogin.php?");
+    VerifyLogin.send();
+
+    
+
+}, false);
+
+// document.addEventListener('DOMContentLoaded', 
+// function VerificarLogin()
+// {
+//     VerifyLogin.onload = function VerificarLogin(){
+//         sesion = VerifyLogin.open("GET", "VerificarLogin.php?");
+//         VerifyLogin.send();
+//      if (session == 0)
+//      {
+//         window.location.replace("./login.html");
+//      }
+// }
+
+// }, false);
--- VerificarLogin.php
+++ VerificarLogin.php
@@ -1,13 +1,23 @@
 <?php
+    // session_start(); 
+    // if(isset($_SESSION['id_us'])) $id_usuario = $_SESSION['id_us'];
+    // if(($id_usuario) != '0'){
+    //     echo json_encode(array(
+    //         'id_us' => $id_usuario,
+    //         'id_name' => $_SESSION['id_name'],
+    //         'id_email' => $_SESSION['id_email']
+    //     ));
+    // }else{
+    //     echo json_encode(array('id_us' => 0));
+    // }
+
     session_start(); 
-    if(isset($_SESSION['id_us'])) $id_usuario = $_SESSION['id_us'];
-    if(($id_usuario) != '0'){
-        echo json_encode(array(
-            'id_us' => $id_usuario,
-            'id_name' => $_SESSION['id_name'],
-            'id_email' => $_SESSION['id_email']
-        ));
-    }else{
-        echo json_encode(array('id_us' => 0));
+    if (isset($_SESSION['id_us']))
+    {
+        echo 1;
+    }
+    else
+    {
+        echo 0;
     }
 ?>
