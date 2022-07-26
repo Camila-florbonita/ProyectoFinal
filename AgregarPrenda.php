07a3bdf026d14a92c275687595dcff94de91f1da
Camila-florbonita <fotosdecamilarock67@gmail.com>
2022-07-26 00:13:49 -0500

    Adjuntar imagen al agregar prenda

--- AgregarPrenda.php
+++ AgregarPrenda.php
@@ -21,103 +21,69 @@ $TS = $_POST["ns"];
 $TM = $_POST["nm"];
 $TL = $_POST["nl"];
 $TXL = $_POST["nxl"];
+$archivo = $_FILES['archivo']['name'];
 
-// //if (isset($_POST['subir'])) {
-//     //Recogemos el archivo enviado por el formulario
-//     $archivo = $_FILES['archivo']['name'];
-//     //Si el archivo contiene algo y es diferente de vacio
-//     if (isset($archivo) && $archivo != "") {
-//        //Obtenemos algunos datos necesarios sobre el archivo
-//        $tipo = $_FILES['archivo']['type'];
-//        $tamano = $_FILES['archivo']['size'];
-//        $temp = $_FILES['archivo']['tmp_name'];
-//        //Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
-//       if (!((strpos($tipo, "gif") || strpos($tipo, "jpeg") || strpos($tipo, "jpg") || strpos($tipo, "png")) && ($tamano < 2000000))) {
-//          echo '<div><b>Error. La extensión o el tamaño de los archivos no es correcta.<br/>
-//          - Se permiten archivos .gif, .jpg, .png. y de 200 kb como máximo.</b></div>';
-//       }
-//       else {
-//          //Si la imagen es correcta en tamaño y tipo
-//          //Se intenta subir al servidor
-//          if (move_uploaded_file($temp, 'images/'.$archivo)) {
-//              //Cambiamos los permisos del archivo a 777 para poder modificarlo posteriormente
-//              chmod('images/'.$archivo, 0777);
-//              //Mostramos el mensaje de que se ha subido co éxito
-//              echo '<div><b>Se ha subido correctamente la imagen.</b></div>';
-//              //Mostramos la imagen subida
-//              echo '<p><img src="images/'.$archivo.'"></p>';
-//          }
-//          else {
-//             //Si no se ha podido subir la imagen, mostramos un mensaje de error
-//             echo '<div><b>Ocurrió algún error al subir el fichero. No pudo guardarse.</b></div>';
-//          }
-//        }
-//     }
-//  //}
-
-$ingreso = "INSERT into productos (nombre_producto, genero, estilo, edad, color, tipo_prenda, temporada, ocasion, formalidad, material, corte, precio, descripcion, precio_oferta) 
-VALUES ('$nombreprenda', '$genero', '$estilo', '$edad', '$color', '$tipodeprenda', '$temporada', '$ocasion', '$formalidad', '$material', '$corte','$precio', '$descprenda', 0)";
-mysqli_query($conexion, $ingreso);
-
-$query = "SELECT id_producto from productos ORDER BY id_producto DESC LIMIT 1";
-$result = mysqli_query($conexion, $query); 
-$registro = mysqli_fetch_array($result);
-$idProducto = $registro['id_producto'];
-
-$ingresoTallas = "INSERT into tallas (id_producto, XS, S, M, L, XL) VALUES ('$idProducto', '$TXS', '$TS', '$TM', '$TL', '$TXL')";
-mysqli_query($conexion, $ingresoTallas);
-
-// $fileName = $_FILES['nameInput']['name'];
-//    $temp = $_FILES['nameInput']['tmp_name'];
-//    $rootFolder = 'ImagenesPrendas/';
-//    $rootFile = $rootFolder.$idProducto.'.jpg';
-//    move_uploaded_file($temp,$rootFolder.$fileName); // o $rootFolder.XD
-//    $query = "INSERT INTO productos (imagen) VALUES ('$rootFile')";
-//    mysqli_query($conexion, $query);
-
-   if ($_FILES["archivo"]["error"] > 0)
+if (isset($archivo) && $archivo != "")
 {
-   echo "Error al cargar el archivo";
-}
-else
-{
-   $acceptedFiles = array("image/jpg");
-   $limite_kb = 3000;
-
-   if (in_array($_FILES["archivo"]["type"], $acceptedFiles) && $_FILES["archivo"]["size"] <= $limite_kb * 1024)
+   $tipo = $_FILES['archivo']['type'];
+   if (!(strpos($tipo,'jpg') || strpos($tipo,'jpeg'))) // || strpost($tipo,'png') || strpost($tipo,'jpeg') || 
    {
-      $ruta = 'ImagenesPrendas/';
-      $fileName = $ruta.$id_producto.'.jpg';
-
-      if(!file_exists($ruta))
-      {
-         mkdir($ruta);
-      }
-
-      if (!file_exists($fileName))
-      {
-         $result = @move_uploaded_file($_FILES["archivo"]["tmp_name"],$fileName);
-
-         if($result)
-         {
-            echo "Archivo Guardado";
-         }
-         else
-         {
-            echo "Error al guardar el archivo";
-         }
-      }
-      else
-      {
-         echo "Archivo ya existe";
-      }
+      echo "<script> alert('Solo se permiten archivos tipo imagen con las extenciones jpg') </script>";
    }
    else
    {
-      echo "Archivo no permitido o excede el tamaño";
+      $ingreso = "INSERT into productos (nombre_producto, genero, estilo, edad, color, tipo_prenda, temporada, ocasion, formalidad, material, corte, precio, descripcion, precio_oferta, ruta) 
+      VALUES ('$nombreprenda', '$genero', '$estilo', '$edad', '$color', '$tipodeprenda', '$temporada', '$ocasion', '$formalidad', '$material', '$corte','$precio', '$descprenda', 0, '')";
+      mysqli_query($conexion, $ingreso);
+      
+      $query = "SELECT id_producto from productos ORDER BY id_producto DESC LIMIT 1";
+      $result = mysqli_query($conexion, $query); 
+      $registro = mysqli_fetch_array($result);
+      $idProducto = $registro['id_producto'];
+      
+      $ingresoTallas = "INSERT into tallas (id_producto, XS, S, M, L, XL) VALUES ('$idProducto', '$TXS', '$TS', '$TM', '$TL', '$TXL')";
+      mysqli_query($conexion, $ingresoTallas);
+      
+      $archivo = $_FILES['archivo']['name'];
+      
+      if (isset($archivo) && $archivo != "")
+      {
+         $tipo = $_FILES['archivo']['type'];
+         $temporal_name = $_FILES['archivo']['tmp_name'];
+      
+         if (!(strpos($tipo,'jpg') || strpos($tipo,'jpeg'))) // || strpost($tipo,'png') || strpost($tipo,'jpeg') || 
+         {
+            echo "<script> alert('Solo se permiten archivos tipo imagen con las extenciones jpg') </script>"; //png y jpeg
+         }
+         else
+         {
+            $ruta = 'ImagenesPrendas'.'/'.$idProducto.'.jpg';
+            $file_query = "INSERT INTO productos (ruta) VALUES ('$ruta') WHERE id_producto = $idProducto";
+            $file_query = "UPDATE productos SET ruta = '$ruta' WHERE id_producto = $idProducto";
+            $resultado = mysqli_query($conexion, $file_query);
+            if ($resultado)
+            {
+               move_uploaded_file($temporal_name, 'ImagenesPrendas'.'/'.$idProducto.'.jpg');
+               echo "<script> alert('Subido con éxtio') </script> ";
+            }
+            else
+            {
+               echo "<script> alert('Ocurrió un error al subir el archivo intente de nuevo') </script>";
+            }
+         }
+      
+      }
+      else
+            {
+               echo "<script> alert('Ocurrió un error intente de nuevo') </script>"; 
+            }
+
    }
 }
 
-header("Location: AgregarPrenda.html");
+
+
+echo "<script> window.location.href = 'AgregarPrenda.html' </script>"; 
+
 ?>
 
