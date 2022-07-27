ff67e135ba81195814496b6c88e5c03d09d5c5b6
Camila-florbonita <fotosdecamilarock67@gmail.com>
2022-07-27 00:52:21 -0500

    enviar correo a los usuarios con productos en la lista de deseos al abastecer el producto

--- enviarCorreo.php
+++ enviarCorreo.php
@@ -4,11 +4,11 @@ session_start();
 
 include "database.php";
 
-$id_usuario = $_SESSION['id_us'];
-$id_name = $_SESSION['id_name'];
-$dato = "SELECT correo_electronico FROM usuarios WHERE id_usuario = '$id_usuario'";
-$dato = "SELECT nombre_usuario FROM usuarios WHERE id_usuario = '$id_usuario'";
-$resname = mysqli_query($conexion, $dato);
+// $id_usuario = $_SESSION['id_us'];
+// $id_name = $_SESSION['id_name'];
+// $dato = "SELECT correo_electronico FROM usuarios WHERE id_usuario = '$id_usuario'";
+// $dato = "SELECT nombre_usuario FROM usuarios WHERE id_usuario = '$id_usuario'";
+// $resname = mysqli_query($conexion, $dato);
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -33,18 +33,20 @@ try {
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('a18300421@ceti.mx', 'Cadivie');
-    $mail->addAddress($id_usuario, $id_name);     //Add a recipient
+    $mail->setFrom('cadivie.ecommerce@gmail.com', 'Cadivie');
+    $mail->addAddress($correo_electronico, $nombre_usuario);     //Add a recipient
     
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Recibo de compra en Cadivie';
-
-    $cuerpo = '<h4>Gracias por su compra</h4>';
-    $cuerpo .= 'El ID de su compra es <b>' . $id_transaccion . '</b></p>';
+    $mail->Subject = '¡Uno de los Productos en su lista de deseos ha sido abastecido!';
+    
+    $cuerpo = '<h4>Ahora puede comprar el producto:</h4>';
+    $cuerpo .= '<p><b>' . $nombre_producto . '</b></p><br>';
+    $cuerpo .= '<p>' . $descripcion . '</p>';
+    // proximamente la imagen
 
     $mail->Body    = utf8_decode($cuerpo);
-    $mail->AltBody = 'Detalles de su compra';
+    $mail->AltBody = 'Producto de lista de deseos';
 
     $mail->setLanguage('es', '/phpmailer/language/phpmailer.lang-es.php');
     
